9445d9e19d11268fdc19500ac8ed7533193bac37
Phvnux <dvnean@gmail.com>
2016-03-14 21:02:47 +0800

    单链表排序 V2.0

--- linlist.cpp
+++ linlist.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+typedef int DataType;
 
 typedef struct Node {
-	int data;
+	DataType data;
 	struct Node *next;
 }SLNode;
 const int ArSize = 10;
 
 SLNode *create();
 int ListLength(SLNode *head);
+void sequencer(SLNode **head, SLNode *end);
 
 int main() {
 	SLNode *linlist;
@@ -20,8 +22,9 @@ int main() {
 
 	linlist = create();
 	size = ListLength(linlist);
-	cout << "size of linlist is: " << size << endl;
-	// system("pause");
+	cout << endl << "size of linlist is: " << size << endl;
+	sequencer(&linlist, NULL);
+    // system("pause");
 	return 0;
 }
 
@@ -29,33 +32,17 @@ SLNode *create() {
 	SLNode *head, *p, *s;
 	int x;
 	int length;
-	length = 100;
+	length = ArSize;
 	head = (SLNode*)malloc(sizeof(Node));//malloc()函数向系统申请分配一个节点
 	head->next = NULL;
 	p = head;
 	cout << "生成链表：" << endl;
 	for (int i = 0; i < length; ++i) {
-		/*		cout << "\nPlease input the data: ";
-				cin >> x;
-
-				if (x != 1) {
-					s = (SLNode*)malloc(sizeof(Node));
-					s->data = x;
-					cout << s->data;
-					p->next = s;
-					p = s;
-				}
-				else
-					cycle = 0;
-			}
-			head = head->next;
-			p->next = NULL;
-			cout << "\n	yyy " << head->data << endl;
-		*/
-		x = rand();
+		
+		x = rand() % 100 + 1;
 		s = (SLNode*)malloc(sizeof(Node));
 		s->data = x;
-		cout << s->data << endl;
+		cout << s->data << " ";
 		p->next = s;
 		p = s;
 	}
@@ -75,4 +62,55 @@ int ListLength(SLNode *head)
 		++size;
 	}
 	return size;
+}
+/****************************************
+**函数名：
+**输入：
+**输出：
+**返回值：
+**作者：
+**时间：
+**修改者：
+**修改时间：
+**版本号：
+****************************************/
+void sequencer(SLNode **head, SLNode *end){
+    SLNode *right;
+    SLNode **left_shift, **right_shift;
+    SLNode *refer, *prev;
+    int count, left_count = 0, right_count = 0;
+    if(*head == end)
+        return;
+    do{
+        refer = *head;
+        left_shift = head;
+        right_shift = &right;
+        for(prev = (*head)->next;; prev != end; prev = prev->next){
+            if(prev->data < refer->data){
+                ++left_count;
+                *left_shift = prev;
+                left_shift = &(prev->next);
+            }
+            else{
+                ++right_count;
+                *right_shift = prev;
+                right_shift = &(prev -> next);
+            }
+        }
+        
+        *right_shift = end;
+        *left_shift = refer;
+        refer->next = right;
+        
+        if(left_shift > right_shift){
+            sequencer(&(refer->next), end);
+            end = refer;
+            count = left_count;
+        }
+        else{
+            sequencer(head, refer);
+            head = &(refer->next);
+            count = right_count;
+        }
+    }while(count > 1);
 }
