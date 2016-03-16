8ec8131352f73d391e2516abb0a4654373a86b51
Phvnux <dvnean@gmail.com>
2016-03-16 14:57:50 +0800

    单链表排序 V3.0 原创

--- linlist.cpp
+++ linlist.cpp
@@ -10,21 +10,25 @@ typedef struct Node {
 const int ArSize = 10;
 
 SLNode *create();
-int ListLength(SLNode *head);
-void sequencer(SLNode **head, SLNode *end);
+void List(SLNode *head);
+//void sequencer(SLNode **head, SLNode *end);
+void sequen_one(SLNode *head, int residue);
 
 int main() {
 	SLNode *linlist;
 	int size = 0;
-//	int length;
-//	int series[ArSize];
-//	length = rand();
+	//	int length;
+	//	int series[ArSize];
+	//	length = rand();
 
 	linlist = create();
-	size = ListLength(linlist);
-	cout << endl << "size of linlist is: " << size << endl;
-	sequencer(&linlist, NULL);
-    // system("pause");
+    cout << "生成链表：" << endl;
+	List(linlist);
+	sequen_one(linlist, ArSize);
+//	sequencer(&linlist, NULL);
+    cout << "链表排序：" << endl;
+	List(linlist);
+//	 system("pause");
 	return 0;
 }
 
@@ -34,35 +38,87 @@ SLNode *create() {
 	int length;
 	length = ArSize;
 	head = (SLNode*)malloc(sizeof(Node));//malloc()函数向系统申请分配一个节点
+	head->data = 0;
 	head->next = NULL;
 	p = head;
-	cout << "生成链表：" << endl;
+//	cout << "生成链表：" << endl;
 	for (int i = 0; i < length; ++i) {
-		
+
 		x = rand() % 100 + 1;
 		s = (SLNode*)malloc(sizeof(Node));
 		s->data = x;
-		cout << s->data << " ";
+//		cout << s->data << " ";
 		p->next = s;
 		p = s;
 	}
-//	head = head->next;
-//	head->data = length;
+	//	head = head->next;
+	//	head->data = length;
 	p->next = NULL;
-//	cout << "头节点：" << head->data << endl;
+	//	cout << "头节点：" << head->data << endl;
 	return (head);
 }
 
-int ListLength(SLNode *head)
+void List(SLNode *head)
 {
 	SLNode *p = head;
 	int size = 1;
+//	cout << "生成链表：\n";
 	while (p->next != NULL) {
 		p = p->next;
+		cout << p->data << " ";
 		++size;
 	}
-	return size;
+	cout << endl;
+//	cout << "链表长度：" << size << endl;
 }
+
+/****************************************
+**函数名：sequen_one
+**输入：SLNode * head, int residue
+**输出：SLNode * linlist
+**返回值：void
+**作者：Dvnean
+**时间：2016/3/16/14:30
+**修改者：----------
+**修改时间：----------
+**版本号：v1.0
+****************************************/
+void sequen_one(SLNode * head, int residue)
+{
+	SLNode * head_s;
+	SLNode *povit, *shift, *current;	//基准结点，右移结点和参照结点
+	int left_count = 0, right_count = 0;	//左右链表计数
+	head_s = head;
+	povit = head_s->next;
+	shift = povit->next;
+	current = povit;
+	int povit_value = povit->data;
+	for (int i = 1; i < residue; ++i) {		
+		if (shift->data <= povit_value) {	//右移结点值小等于基准值，将该结点移动到头节点的下一个结点
+			current->next = shift->next;
+			shift->next = head_s->next;
+			head_s->next = shift;
+			left_count++;
+			shift = current->next;
+		}
+		else {								//右移结点大于基准值，右移结点和参照结点右移
+			current = shift;
+			shift = shift->next;
+			right_count++;
+		}
+	}
+
+	//递归
+	if (left_count > 1) {					
+		sequen_one(head_s, left_count);
+	}
+	if (right_count > 1) {
+		sequen_one(povit, right_count);
+	}
+
+}
+
+
 /****************************************
 **函数名：
 **输入：
@@ -74,43 +130,46 @@ int ListLength(SLNode *head)
 **修改时间：
 **版本号：
 ****************************************/
-void sequencer(SLNode **head, SLNode *end){
-    SLNode *right;
-    SLNode **left_shift, **right_shift;
-    SLNode *refer, *prev;
-    int count, left_count = 0, right_count = 0;
-    if(*head == end)
-        return;
-    do{
-        refer = *head;
-        left_shift = head;
-        right_shift = &right;
-        for(prev = (*head)->next;; prev != end; prev = prev->next){
-            if(prev->data < refer->data){
-                ++left_count;
-                *left_shift = prev;
-                left_shift = &(prev->next);
-            }
-            else{
-                ++right_count;
-                *right_shift = prev;
-                right_shift = &(prev -> next);
-            }
-        }
-        
-        *right_shift = end;
-        *left_shift = refer;
-        refer->next = right;
-        
-        if(left_shift > right_shift){
-            sequencer(&(refer->next), end);
-            end = refer;
-            count = left_count;
-        }
-        else{
-            sequencer(head, refer);
-            head = &(refer->next);
-            count = right_count;
-        }
-    }while(count > 1);
+/*
+void sequencer(SLNode **head, SLNode *end) {
+	SLNode *right;
+	SLNode **left_shift, **right_shift;
+	SLNode *refer, *prev;
+	int count, left_count = 0, right_count = 0;
+	if (*head == end)
+		return;
+	do {
+		refer = *head;
+		left_shift = head;
+		right_shift = &right;
+		for (prev = (*head)->next; prev != end; prev = prev->next) {
+			if (prev->data < refer->data) {
+				++left_count;
+				*left_shift = prev;
+				left_shift = &(prev->next);
+			}
+			else {
+				++right_count;
+				*right_shift = prev;
+				right_shift = &(prev->next);
+			}
+		}
+
+		*right_shift = end;
+		*left_shift = refer;
+		refer->next = right;
+
+		if (left_shift > right_shift) {
+			sequencer(&(refer->next), end);
+			end = refer;
+			count = left_count;
+		}
+		else {
+			sequencer(head, refer);
+			head = &(refer->next);
+			count = right_count;
+		}
+	} while (count > 1);
+	
 }
+*/
