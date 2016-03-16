9c950c95d7e36205962d748accb8e643bd97b494
Phvnux <dvnean@gmail.com>
2016-03-16 17:19:28 +0800

    单链表排序 V4.0 加冒泡排序

--- linlist.cpp
+++ linlist.cpp
@@ -1,37 +1,56 @@
 #include <iostream>
 #include <stdlib.h>
+#include <ctime>
 using namespace std;
 typedef int DataType;
+const int ArSize = 10;
 
 typedef struct Node {
 	DataType data;
 	struct Node *next;
 }SLNode;
-const int ArSize = 10;
 
 SLNode *create();
 void List(SLNode *head);
 //void sequencer(SLNode **head, SLNode *end);
 void sequen_one(SLNode *head, int residue);
+void sequen_two(SLNode *head, int size);
+
 
 int main() {
 	SLNode *linlist;
+	SLNode *linlist_s;
 	int size = 0;
 	//	int length;
 	//	int series[ArSize];
 	//	length = rand();
 
 	linlist = create();
-    cout << "生成链表：" << endl;
+	cout << "=================================\n" << "创建单链表：\n";
 	List(linlist);
+	cout << "快速排序：\n";
+    clock_t q1_start_time=clock();
 	sequen_one(linlist, ArSize);
+    clock_t q1_end_time=clock();
 //	sequencer(&linlist, NULL);
-    cout << "链表排序：" << endl;
 	List(linlist);
-//	 system("pause");
+    cout<< "排序用时: "<<static_cast<double>(q1_end_time-q1_start_time)/CLOCKS_PER_SEC*1000<<"ms"<<endl;//输出运行时间
+	cout << "=================================\n" << "创建单链表：\n";
+	linlist_s = create();
+	List(linlist_s);
+	cout << "冒泡排序：\n";
+	clock_t q2_start_time=clock();
+    sequen_two(linlist_s, ArSize);
+    clock_t q2_end_time=clock();
+    
+	List(linlist_s);
+    cout<< "排序用时: "<<static_cast<double>(q2_end_time-q2_start_time)/CLOCKS_PER_SEC*1000<<"ms"<<endl;//输出运行时间
+    cout << "=================================\n";
+//	system("pause");
 	return 0;
 }
 
+//创建单链表
 SLNode *create() {
 	SLNode *head, *p, *s;
 	int x;
@@ -58,6 +77,7 @@ SLNode *create() {
 	return (head);
 }
 
+//输出单链表
 void List(SLNode *head)
 {
 	SLNode *p = head;
@@ -74,6 +94,7 @@ void List(SLNode *head)
 
 /****************************************
 **函数名：sequen_one
+**描述：单链表快速排序
 **输入：SLNode * head, int residue
 **输出：SLNode * linlist
 **返回值：void
@@ -120,16 +141,41 @@ void sequen_one(SLNode * head, int residue)
 
 
 /****************************************
-**函数名：
-**输入：
-**输出：
-**返回值：
-**作者：
-**时间：
-**修改者：
-**修改时间：
-**版本号：
+**函数名：sequen_two
+**描述：单链表冒泡排序
+**输入：SLNode *head, int size
+**输出：linlist_s
+**返回值：void
+**作者：Dvnean
+**时间：2016/3/16/16:43
+**修改者：----------
+**修改时间：----------
+**版本号：v1.0
 ****************************************/
+void sequen_two(SLNode *head, int size) {
+	SLNode *shift, *min_pos, *current, *prev;
+	current = min_pos = head->next;
+	shift = min_pos->next;
+	prev = head;
+	for (int i = 1; i < size; ++i ) {
+		
+		if (min_pos->data > shift->data) {
+			min_pos = shift;
+			prev = current;
+		}
+		shift = shift->next;
+		current = current->next;
+	}
+	prev->next = min_pos->next;
+	min_pos->next = head->next;
+	head->next = min_pos;
+
+	if (min_pos->next != NULL) {
+		sequen_two(min_pos, size-1);
+	}
+}
+
+
 /*
 void sequencer(SLNode **head, SLNode *end) {
 	SLNode *right;
@@ -142,7 +188,8 @@ void sequencer(SLNode **head, SLNode *end) {
 		refer = *head;
 		left_shift = head;
 		right_shift = &right;
-		for (prev = (*head)->next; prev != end; prev = prev->next) {
+		prev = (*head)->next;
+		for (; prev != end && prev != NULL; prev = prev->next) {
 			if (prev->data < refer->data) {
 				++left_count;
 				*left_shift = prev;
@@ -172,4 +219,5 @@ void sequencer(SLNode **head, SLNode *end) {
 	} while (count > 1);
 	
 }
+
 */
