a65d6320e2a671668e5390aba4f9c1235f259507
Phvnux <Dvnean@gmail.com>
2016-11-02 21:35:03 +0800

    this指针

---
+++ Stock20/stock20.h
@@ -0,0 +1,30 @@
+//stock20.h 
+//version 10
+
+#ifndef STOCK00_H_
+#define STOCK00_H_
+
+#include <String>
+
+class Stock{
+    private:
+        std::string company;
+        long shares;
+        double share_val;
+        double total_val;
+        void set_tot() { total_val = shares * share_val; }
+    public:
+        // void acquire(const std::string & co, long n, double pr);
+        //two constructors
+        Stock();    //default constructor
+        Stock(const std::string & co, long n = 0, double pr = 0.0);
+        ~Stock();   //析构函数
+        void buy(long num, double price);
+        void sell(long num, double price);
+        void update(double price);
+        void show() const;
+        const Stock & topval(const Stock & s) const;
+        
+};
+
+#endif
